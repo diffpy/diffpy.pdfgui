67e2dcdf5b2b883009227af969ad81d35c1a22cf
Pavol Juhas <pavol.juhas@gmail.com>
2009-03-25 22:52:00 +0000

    Main kickstart script moved into diffpy.pdfgui package to make it compatible with setuptools entry points.

---
+++ diffpy/pdfgui/applications/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy.pdfgui     by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2009 Trustees of the Columbia University
+#                   in the City of New York.  All rights reserved.
+#
+# File coded by:    Pavol Juhas
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
