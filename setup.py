8e36c59636bf8bd3b8e7aec8c589a431ee100adc
Pavol Juhas <pavol.juhas@gmail.com>
2009-04-24 01:20:22 +0000

    Added package metadata to setup.py files.

--- setup.py
+++ setup.py
@@ -47,10 +47,27 @@ setup(
 
         author = 'Simon J.L. Billinge',
         author_email = 'sb2896@columbia.edu',
-        description = "GUI for PDF simulation and structure refinement.",
-        license = 'BSD',
+        maintainer = 'Pavol Juhas',
+        maintainer_email = 'pj2192@columbia.edu',
         url = 'http://www.diffpy.org/',
+        download_url = 'http://www.diffpy.org/packages/',
+        description = "GUI for PDF simulation and structure refinement.",
+        license = open('LICENSE.txt').read(),
         keywords = 'PDF structure refinement GUI',
+        classifiers = [
+            # List of possible values at
+            # http://pypi.python.org/pypi?:action=list_classifiers
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: MacOS X',
+            'Environment :: Win32 (MS Windows)',
+            'Environment :: X11 Applications',
+            'Intended Audience :: Science/Research',
+            'Operating System :: MacOS',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Programming Language :: Python :: 2.5',
+            'Topic :: Scientific/Engineering :: Physics',
+        ],
 )
 
 # End of file
