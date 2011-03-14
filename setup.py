d2731818ed9a21c379bf716aa3ab95a58931ef4d
Pavol Juhas <pavol.juhas@gmail.com>
2011-03-14 17:08:01 +0000

    gridutils move finished here.

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         install_requires = [
             'diffpy.Structure>=1.0-r3050',
             'diffpy.pdffit2>=1.0-r3050',
+            'diffpy.utils>=1.0-r6330',
         ],
         dependency_links = [
             'http://www.diffpy.org/packages/',
