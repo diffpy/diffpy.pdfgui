35f35a3c4f3e58539b097e27203051679d7dae84
Pavol Juhas <pavol.juhas@gmail.com>
2016-02-20 17:38:32 -0500

    Add script for resolving paths for Windows menu.
    Determine diffpy.pdfgui installation directory at conda build time.

---
+++ conda-recipe/expandpdfguibase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from pkg_resources import Requirement, resource_filename
+
+pkg = Requirement.parse('diffpy.pdfgui')
+PDFGUIPATH = resource_filename(pkg, '')
+assert PDFGUIPATH.lower().startswith(sys.prefix.lower())
+PDFGUIBASE = PDFGUIPATH[len(sys.prefix):].replace('\\', '/').strip('/')
+
+if __name__ == "__main__":
+    content = open(sys.argv[1]).read()
+    output = content.replace('@PDFGUIBASE@', PDFGUIBASE)
+    sys.stdout.write(output)
