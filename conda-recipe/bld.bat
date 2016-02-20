1dbaa32aab9052c708f0c0c1a9b85e053266f557
Pavol Juhas <pavol.juhas@gmail.com>
2016-02-19 19:14:52 -0500

    Add Windows start menu in conda recipe.
    Create Windows menu group "DiffPy" with PDFgui-related items.

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,9 +1,19 @@
-"%PYTHON%" setup.py install
+%PYTHON% setup.py install
 if errorlevel 1 exit 1
 
 :: Add more build steps here, if they are necessary.
 
-"%PYTHON%" setup.py --version > __conda_version__.txt
+%PYTHON% setup.py --version > __conda_version__.txt
+
+set MENU_DIR=%PREFIX%\Menu
+if not exist (%MENU_DIR%) mkdir %MENU_DIR%
+
+cd %RECIPE_DIR%
+%PYTHON% expandpdfguibase.py menu-windows.json > %MENU_DIR%\menu-windows.json
+if errorlevel 1 exit 1
+
+copy pdfgui.ico %MENU_DIR%\
+if errorlevel 1 exit 1
 
 :: See http://docs.continuum.io/conda/build.html
 :: for a list of environment variables that are set during the build process.
