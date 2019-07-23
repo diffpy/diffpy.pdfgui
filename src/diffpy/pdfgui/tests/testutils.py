7e7c72fac8c5d5133c107799076bcc09d79c0984
Pavol Juhas <pavol.juhas@gmail.com>
2019-07-23 17:18:25 -0400

    TST: add overridewebbrowser helper function
    Context manager to temporarily override `webbrowser.open`.

--- src/diffpy/pdfgui/tests/testutils.py
+++ src/diffpy/pdfgui/tests/testutils.py
@@ -16,11 +16,32 @@
 """Helper routines for running other unit tests.
 """
 
+from contextlib import contextmanager
+
+
 # helper functions
 
 def datafile(filename):
     from pkg_resources import resource_filename
     rv = resource_filename(__name__, "testdata/" + filename)
     return rv
+
+
+@contextmanager
+def overridewebbrowser(fnc_open):
+    "Temporarily replace `webbrowser.open` with given function."
+    import webbrowser
+    controller = webbrowser.get()
+    save_open = controller.open
+    def open_override(url, new=0, autoraise=True):
+        fnc_open(url)
+        return True
+    controller.open = open_override
+    try:
+        yield save_open
+    finally:
+        del controller.open
+        assert controller.open == save_open
+    pass
 
 # End of file
