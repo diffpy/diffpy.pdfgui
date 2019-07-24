bba709ed7d0cdbfb4d34f2eb294eb196cf38f35a
Pavol Juhas <pavol.juhas@gmail.com>
2019-07-24 02:14:04 -0400

    TST: add stub test of PhaseConfigurePanel

---
+++ src/diffpy/pdfgui/tests/testphaseconfigurepanel.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy.pdfgui     Complex Modeling Initiative
+#                   (c) 2018 Brookhaven Science Associates,
+#                   Brookhaven National Laboratory.
+#                   All rights reserved.
+#
+# File coded by:    Pavol Juhas
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
+
+"""
+Unit tests for PhaseConfigurePanel class.
+"""
+
+import unittest
+
+import wx
+
+from diffpy.pdfgui.gui.phaseconfigurepanel import PhaseConfigurePanel
+from diffpy.pdfgui.control.fitstructure import FitStructure
+from diffpy.pdfgui.tests.testutils import GUITestCase
+
+# ----------------------------------------------------------------------------
+
+class TestPhaseConfigurePanel(GUITestCase):
+
+    def setUp(self):
+        self.app = wx.App()
+        self.frame = wx.Frame(None)
+        self.panel = PhaseConfigurePanel(self.frame)
+        self.panel.structure = FitStructure("stru")
+        self.panel.refresh()
+        self.panel.mainFrame = _TMainFrame()
+        self.frame.window = self.panel
+        return
+
+    def tearDown(self):
+        self.frame.Close()
+        self.app.Destroy()
+        return
+
+
+    def test_ok(self):
+        "FIXME - temporary dummy test"
+        self.assertTrue(True)
+        return
+
+# End of class TestPhaseConfigurePanel
+
+# Local Helpers --------------------------------------------------------------
+
+# TODO - refactor into GUITestCase
+
+class _TMainFrame(object):
+    "Think mockup of the used MainFrame methods."
+
+    altered = False
+
+    def needsSave(self):
+        self.altered = True
+        return
+
+# ----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    unittest.main()
