2a750e067d853d8b0e2c1a31c7a59ad1e740b01d
Ethan <ethanp611@gmail.com>
2021-09-15 13:17:39 -0600

    BUG: Fixed addition/deletion of mag atoms
    Magnetic atoms can now be added with others deleted at the same time.

--- src/diffpy/pdfgui/gui/magconfigurepanel.py
+++ src/diffpy/pdfgui/gui/magconfigurepanel.py
@@ -451,33 +451,29 @@ class MagConfigurePanel(wx.Panel, PDFPanel):
             X, orig_inx, nonmag, Xelem = split_up_magnetics(
                 np.arange(1, 1+len(struc)), mags, struc, row_element)
 
-            if self.firstViewerLaunch is False:
+            if self.firstViewerLaunch is False:  # Checks if changes to Mag Structure have been made
                 XarrTmp = self.Xarr
-                if len(X) != len(self.Xarr):
-                    if len(X) > len(self.Xarr):
-                        for i in range(len(X)):
-                            isInArr = False
-                            for j in range(len(self.Xarr)):
-                                if X[i, 0] == self.Xarr[j, 0] and X[i, 1] == self.Xarr[j, 1] and X[i, 2] == self.Xarr[j, 2]:
-                                    isInArr = True
-                            if isInArr is False:
-                                Xtmp = np.array(
-                                    [X[i, 0], X[i, 1], X[i, 2], 0, 0, 0, 0, 0, 0])
-                                index = len(XarrTmp) + 1
-                                self.Xarr = np.append(
-                                    self.Xarr, [Xtmp], axis=0)
-                    elif len(X) < len(self.Xarr):
-                        toDelete = []
-                        for i in range(len(self.Xarr)):
-                            isInArr = False
-                            for j in range(len(X)):
-                                if self.Xarr[i, 0] == X[j, 0] and self.Xarr[i, 1] == X[j, 1] and self.Xarr[i, 2] == X[j, 2]:
-                                    isInArr = True
-                            if isInArr is False:
-                                toDelete.append(i)
-                        self.Xarr = np.delete(self.Xarr, toDelete, axis=0)
-
-                        #X = self.Xarr
+                if X != self.Xarr:
+                    toDelete = []
+                    for i in range(len(X)): # Checks for added mag atoms
+                        isInArr = False
+                        for j in range(len(self.Xarr)):
+                            if X[i, 0] == self.Xarr[j, 0] and X[i, 1] == self.Xarr[j, 1] and X[i, 2] == self.Xarr[j, 2]:
+                                isInArr = True
+                        if isInArr is False:
+                            Xtmp = np.array(
+                                [X[i, 0], X[i, 1], X[i, 2], 0, 0, 0, 0, 0, 0])
+                            XarrTmp = np.append(
+                                XarrTmp, [Xtmp], axis=0)
+                    self.Xarr = XarrTmp
+                    for i in range(len(self.Xarr)): # Checks for deleted mag atoms
+                        isInArr = False
+                        for j in range(len(X)):
+                            if self.Xarr[i, 0] == X[j, 0] and self.Xarr[i, 1] == X[j, 1] and self.Xarr[i, 2] == X[j, 2]:
+                                isInArr = True
+                        if isInArr is False:
+                            toDelete.append(i)
+                    self.Xarr = np.delete(self.Xarr, toDelete, axis=0)
                 X = np.array(self.Xarr)
                 print(X.shape)
             canvas = CanvasFrame(
