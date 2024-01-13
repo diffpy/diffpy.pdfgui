f06c971ef84b1854951a1b795b60b917a746b119
Sam Andrello <samqandrello@gmail.com>
2024-01-13 11:45:57 -0500

    config file updates to py 3.12

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,15 +2,15 @@
 
 Notable differences from version 1.1.2.
 
-## Version 2.0.4 – 2023-12-09
+## Version 2.0.4 – 2024-01-13
 
 ### Added
 
-- Support for Python 3.11.
+- Support for Python 3.11 and 3.12.
 
 ### Changed
 
-- Update wxpython to 4.2.1
+- Update wxpython to 4.2+
 
 ### Deprecated
 
--- setup.py
+++ setup.py
@@ -147,6 +147,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Scientific/Engineering :: Chemistry',
         'Topic :: Scientific/Engineering :: Physics',
     ],
