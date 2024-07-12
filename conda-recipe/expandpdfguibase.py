4993eebf08aafecd93dc1a4a71ccec15d3441382
Caden Myers <cjm2304@columbia.edu>
2024-07-12 11:19:18 -0400

    black edits from running black on all files

--- conda-recipe/expandpdfguibase.py
+++ conda-recipe/expandpdfguibase.py
@@ -3,13 +3,13 @@
 import sys
 from pkg_resources import Requirement, resource_filename
 
-pkg = Requirement.parse('diffpy.pdfgui')
-PDFGUIPATH = resource_filename(pkg, '')
+pkg = Requirement.parse("diffpy.pdfgui")
+PDFGUIPATH = resource_filename(pkg, "")
 assert PDFGUIPATH.lower().startswith(sys.prefix.lower())
-PDFGUIBASE = PDFGUIPATH[len(sys.prefix):].replace('\\', '/').strip('/')
+PDFGUIBASE = PDFGUIPATH[len(sys.prefix) :].replace("\\", "/").strip("/")
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as fp:
         content = fp.read()
-    output = content.replace('@PDFGUIBASE@', PDFGUIBASE)
+    output = content.replace("@PDFGUIBASE@", PDFGUIBASE)
     sys.stdout.write(output)
