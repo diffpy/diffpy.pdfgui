0b643fc75512bfda84e7559260b3bbce43ac1667
Caden Myers <cjm2304@columbia.edu>
2024-07-12 11:01:42 -0400

    black edits 6

--- src/diffpy/pdfgui/gui/extendedplotframe.py
+++ src/diffpy/pdfgui/gui/extendedplotframe.py
@@ -20,7 +20,8 @@ The module contains extensions for GUI plot frame.
 import os.path
 
 import matplotlib
-matplotlib.use('WXAgg')
+
+matplotlib.use("WXAgg")
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.backends.backend_wxagg import NavigationToolbar2WxAgg as NavToolbar
 from matplotlib.figure import Figure
@@ -31,25 +32,27 @@ import wx
 from diffpy.pdfgui.gui.wxextensions import wx12
 from diffpy.pdfgui.gui.pdfguiglobals import iconpath
 
-DATA_SAVE_ID  = wx12.NewIdRef()
+DATA_SAVE_ID = wx12.NewIdRef()
+
 
 class ExtendedToolbar(NavToolbar):
     """An extended plotting toolbar with a save and close button."""
 
     # override NavToolbar.toolitems to exclude the subplots tool.
-    toolitems = tuple(el for el in NavToolbar.toolitems
-                      if el[0] != 'Subplots')
+    toolitems = tuple(el for el in NavToolbar.toolitems if el[0] != "Subplots")
 
     def __init__(self, canvas):
         NavToolbar.__init__(self, canvas)
         wx12.patchToolBarMethods(self)
         # Load customized icon image
-        save_icon_fp = iconpath('exportplotdata.png')
+        save_icon_fp = iconpath("exportplotdata.png")
         save_icon = wx.Bitmap(save_icon_fp)
         # Add new buttons
-        self.AddTool(DATA_SAVE_ID, "Export data", save_icon,
-                     shortHelp='Export plot data to file')
+        self.AddTool(
+            DATA_SAVE_ID, "Export data", save_icon, shortHelp="Export plot data to file"
+        )
         return
+
 
 # End class ExtendedToolbar
 
@@ -65,9 +68,9 @@ class ExtendedPlotFrame(wx.Frame):
     """
 
     # keyboard shortcut(s) for closing plot window
-    close_keys = set(matplotlib.rcParamsDefault['keymap.quit'])
+    close_keys = set(matplotlib.rcParamsDefault["keymap.quit"])
 
-    def __init__(self, parent = None, *args, **kwargs):
+    def __init__(self, parent=None, *args, **kwargs):
         """Initialize the CanvasFrame.
 
         The frame uses ExtendedToolbar as a toolbar, which has a save data
@@ -77,45 +80,45 @@ class ExtendedPlotFrame(wx.Frame):
         args -- argument list
         kwargs -- keyword argument list
         """
-        wx.Frame.__init__(self,parent,-1,'ExtendedPlotFrame',size=(550,350))
+        wx.Frame.__init__(self, parent, -1, "ExtendedPlotFrame", size=(550, 350))
 
         # figsize in inches
-        self.figure = Figure(figsize=(0.5,0.5), dpi=72)
+        self.figure = Figure(figsize=(0.5, 0.5), dpi=72)
 
         # we will manage view scale ourselves
         self.subplot = self.figure.add_subplot(111, autoscale_on=False)
         self.canvas = FigureCanvas(self, -1, self.figure)
 
         # Introspection data
-        self.dirname = ''
-        self.filename = ''
+        self.dirname = ""
+        self.filename = ""
 
         self.sizer = wx.BoxSizer(wx.VERTICAL)
-        self.sizer.Add(self.canvas, 1, wx.TOP|wx.LEFT|wx.EXPAND)
+        self.sizer.Add(self.canvas, 1, wx.TOP | wx.LEFT | wx.EXPAND)
         self.toolbar = ExtendedToolbar(self.canvas)
         self.toolbar.Realize()
 
-        self.coordLabel = wx.StaticText(self,-1,style = wx.ALIGN_RIGHT|wx.NO_BORDER)
+        self.coordLabel = wx.StaticText(self, -1, style=wx.ALIGN_RIGHT | wx.NO_BORDER)
         # Place coordinates textbox in a horizontal sizer next to the toolbar.
         barSizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.sizer.Add(barSizer, 0, wx.EXPAND|wx.CENTER)
+        self.sizer.Add(barSizer, 0, wx.EXPAND | wx.CENTER)
         barSizer.Add(self.toolbar, 0, wx.CENTER)
-        barSizer.Add((20,10),0)
+        barSizer.Add((20, 10), 0)
         barSizer.Add(self.coordLabel, 0, wx.CENTER)
 
         # update the axes menu on the toolbar
         self.toolbar.update()
         self.SetSizer(self.sizer)
         self.Fit()
-        self.SetSize((600,400))
+        self.SetSize((600, 400))
         # Use toolbar's color for coordinates label background.
         self.SetBackgroundColour(self.toolbar.GetBackgroundColour())
         # FIXME -- toolbar background color does not match on Mac OS X.
         # Use GIMP - picked color until a proper way is found.
-        if wx.Platform == '__WXMAC__':
+        if wx.Platform == "__WXMAC__":
             self.SetBackgroundColour((200, 200, 200, 255))
-        self.canvas.mpl_connect('motion_notify_event', self.UpdateStatusBar)
-        self.canvas.mpl_connect('key_press_event', self.mplKeyPress)
+        self.canvas.mpl_connect("motion_notify_event", self.UpdateStatusBar)
+        self.canvas.mpl_connect("key_press_event", self.mplKeyPress)
         self.Bind(wx.EVT_PAINT, self.OnPaint)
         self.Bind(wx.EVT_TOOL, self.savePlotData, id=DATA_SAVE_ID)
         self.Bind(wx.EVT_CLOSE, self.onClose)
@@ -127,7 +130,7 @@ class ExtendedPlotFrame(wx.Frame):
     # EVENT CODE #############################################################
     def onClose(self, evt):
         """Close the frame."""
-        if hasattr(self, 'plotter'):
+        if hasattr(self, "plotter"):
             self.plotter.onWindowClose()
         self.Destroy()
         return
@@ -138,12 +141,18 @@ class ExtendedPlotFrame(wx.Frame):
 
     def savePlotData(self, evt):
         """Save the data in the plot in columns."""
-        d = wx.FileDialog(None, "Save as...", self.dirname, self.filename,
-                "(*.dat)|*.dat|(*.txt)|*.txt|(*)|*", wx.FD_SAVE|wx.FD_OVERWRITE_PROMPT)
+        d = wx.FileDialog(
+            None,
+            "Save as...",
+            self.dirname,
+            self.filename,
+            "(*.dat)|*.dat|(*.txt)|*.txt|(*)|*",
+            wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT,
+        )
         if d.ShowModal() == wx.ID_OK:
             fullname = d.GetPath()
             self.dirname = os.path.dirname(fullname)
-            self.filename =  os.path.basename(fullname)
+            self.filename = os.path.basename(fullname)
             self.plotter.export(fullname)
 
         d.Destroy()
@@ -155,7 +164,6 @@ class ExtendedPlotFrame(wx.Frame):
             xystr = "x = %g, y = %g" % (x, y)
             self.coordLabel.SetLabel(xystr)
 
-
     def mplKeyPress(self, event):
         """Process keyboard input in matplotlib plot window.
 
@@ -165,10 +173,8 @@ class ExtendedPlotFrame(wx.Frame):
             self.Close()
         return
 
-
     def replot(self):
-        """officially call function in matplotlib to do drawing
-        """
+        """officially call function in matplotlib to do drawing"""
         self.canvas.draw()
 
     def insertCurve(self, xData, yData, style):
@@ -178,7 +184,7 @@ class ExtendedPlotFrame(wx.Frame):
         style -- the way curve should be plotted
         return:  internal reference to the newly added curve
         """
-        stylestr,properties = self.__translateStyles(style)
+        stylestr, properties = self.__translateStyles(style)
         curveRef = self.subplot.plot(xData, yData, stylestr, **properties)[0]
         self.subplot.legend(**legendBoxProperties())
         try:
@@ -208,7 +214,7 @@ class ExtendedPlotFrame(wx.Frame):
         style -- style dictionary
         """
         stylestr, properties = self.__translateStyles(style)
-        #FIXME: we discard stylestr because it seems there's no way
+        # FIXME: we discard stylestr because it seems there's no way
         # it can be changed afterwards.
         setp((curveRef,), **properties)
         self.subplot.legend(**legendBoxProperties())
@@ -224,13 +230,12 @@ class ExtendedPlotFrame(wx.Frame):
         self.__updateViewLimits()
 
     def __updateViewLimits(self):
-        """adjust the subplot range in order to show all curves correctly.
-        """
+        """adjust the subplot range in order to show all curves correctly."""
         # NOTE:
         # we need to adjust view limits by ourselves because Matplotlib can't
         # set the legend nicely when there are multiple curves in the plot.
         # Beside, autoscale can not automatically respond to data change.
-        if len(self.datalims) == 0 :
+        if len(self.datalims) == 0:
             return
         # ignore previous range
         self.subplot.dataLim.ignore(True)
@@ -242,7 +247,7 @@ class ExtendedPlotFrame(wx.Frame):
 
         # If multiple curve, we need calculate new x limits because legend box
         # take up some space
-        #NOTE: 3 and 0.33 is our best estimation for a good view
+        # NOTE: 3 and 0.33 is our best estimation for a good view
         # 2007-10-25 PJ: it is better to use full plot area
         # if len(self.datalims) > 3:
         #     # leave extra room for legend by shift the upper bound for x axis
@@ -258,45 +263,67 @@ class ExtendedPlotFrame(wx.Frame):
 
         style -- general curve style dictionary (defined in demoplot)
         """
-        #Translation dictionary
-        lineStyleDict ={'solid':'-','dash':'--','dot':':','dashDot':'-.'}
-        symbolDict ={'diamond':'d','square':'s','circle':'o',
-        'cross':'+','xCross':'x','triangle':'^'}
-        colorDict = {'blue':'b','green':'g','red':'r','cyan':'c',
-        'magenta':'m','yellow':'y','black':'k','white':'w',
-        'darkRed':'#8B0000', 'darkGreen':'#006400', 'darkCyan':'#008B8B',
-        'darkYellow':'#FFD700','darkBlue':'#00008B','darkMagenta':'#8B008B'}
+        # Translation dictionary
+        lineStyleDict = {"solid": "-", "dash": "--", "dot": ":", "dashDot": "-."}
+        symbolDict = {
+            "diamond": "d",
+            "square": "s",
+            "circle": "o",
+            "cross": "+",
+            "xCross": "x",
+            "triangle": "^",
+        }
+        colorDict = {
+            "blue": "b",
+            "green": "g",
+            "red": "r",
+            "cyan": "c",
+            "magenta": "m",
+            "yellow": "y",
+            "black": "k",
+            "white": "w",
+            "darkRed": "#8B0000",
+            "darkGreen": "#006400",
+            "darkCyan": "#008B8B",
+            "darkYellow": "#FFD700",
+            "darkBlue": "#00008B",
+            "darkMagenta": "#8B008B",
+        }
 
         properties = {}
 
-        #NOTE: matplotlib takes additional string for plotting. It's
+        # NOTE: matplotlib takes additional string for plotting. It's
         # purpose is like 'with' in Gnuplot
-        stylestr  = ''
+        stylestr = ""
         # color is universal for either lines, points or linepoints
-        color = colorDict.get(style['color'], 'k')
+        color = colorDict.get(style["color"], "k")
 
-        if style['with'] in ('points', 'linespoints'):
+        if style["with"] in ("points", "linespoints"):
             # require symbol properties
-            stylestr = '.'
-            symbol = symbolDict.get(style['symbol'],'s') # prefer square
-            symbolSize = style['symbolSize']
-            symbolColor = colorDict.get(style['symbolColor'], 'k')
-            properties.update({#'linewidth':0.0, # doesn't affect any
-                               'markerfacecolor':symbolColor,
-                              'markeredgecolor':color,
-                             'marker':symbol,'markersize':symbolSize})
-        if style['with'] != 'points':
+            stylestr = "."
+            symbol = symbolDict.get(style["symbol"], "s")  # prefer square
+            symbolSize = style["symbolSize"]
+            symbolColor = colorDict.get(style["symbolColor"], "k")
+            properties.update(
+                {  #'linewidth':0.0, # doesn't affect any
+                    "markerfacecolor": symbolColor,
+                    "markeredgecolor": color,
+                    "marker": symbol,
+                    "markersize": symbolSize,
+                }
+            )
+        if style["with"] != "points":
             # not 'points', so line properties are required as well
-            lineStyle = lineStyleDict.get(style['line'],'-') #prefer solid
-            lineWidth = style['width']
+            lineStyle = lineStyleDict.get(style["line"], "-")  # prefer solid
+            lineWidth = style["width"]
             stylestr += lineStyle
-            properties.update({'color':color,'linestyle':lineStyle,
-                             'linewidth':lineWidth})
+            properties.update(
+                {"color": color, "linestyle": lineStyle, "linewidth": lineWidth}
+            )
 
-        if 'legend' in style:
-            properties['label'] = style['legend']
+        if "legend" in style:
+            properties["label"] = style["legend"]
         return stylestr, properties
-
 
     def setTitle(self, wt, gt):
         """set graph labels
@@ -324,8 +351,9 @@ class ExtendedPlotFrame(wx.Frame):
     def clear(self):
         """erase all curves"""
         self.subplot.clear()
-        self.curverefs =[]
+        self.curverefs = []
         self.replot()
+
 
 # End class ExtendedPlotFrame
 
@@ -340,31 +368,34 @@ def legendBoxProperties():
     """
     global _lbp
     # return immediately if properties have already been cached
-    if len(_lbp) > 0:   return _lbp
+    if len(_lbp) > 0:
+        return _lbp
     #  figure out matplotlib version and appropriate names
     from pkg_resources import parse_version
     from matplotlib import __version__ as mplver
-    if parse_version(mplver) >= parse_version('0.98.5'):
+
+    if parse_version(mplver) >= parse_version("0.98.5"):
         _lbp = {
-            'loc' : 'upper right',
-            'numpoints' : 3,        # number of points in the legend line
-            'borderpad' : 0.25,     # whitespace in the legend border
-            'labelspacing' : 0,     # space between legend entries
-            'handlelength' : 1.5,   # the length of the legend lines
-            'handletextpad' : 0.5,  # separation between line and text
-            'prop' : FontProperties(size='medium'),
+            "loc": "upper right",
+            "numpoints": 3,  # number of points in the legend line
+            "borderpad": 0.25,  # whitespace in the legend border
+            "labelspacing": 0,  # space between legend entries
+            "handlelength": 1.5,  # the length of the legend lines
+            "handletextpad": 0.5,  # separation between line and text
+            "prop": FontProperties(size="medium"),
         }
     else:
         _lbp = {
-            'loc' : 'upper right',
-            'numpoints' : 3,        # number of points in the legend line
-            'pad' : 0.20,           # whitespace in the legend border
-            'labelsep' : 0.005,     # space between legend entries
-            'handlelen' : 0.03,     # the length of the legend lines
-            'handletextsep' : 0.02, # separation between line and text
-            'prop' : FontProperties(size='medium'),
+            "loc": "upper right",
+            "numpoints": 3,  # number of points in the legend line
+            "pad": 0.20,  # whitespace in the legend border
+            "labelsep": 0.005,  # space between legend entries
+            "handlelen": 0.03,  # the length of the legend lines
+            "handletextsep": 0.02,  # separation between line and text
+            "prop": FontProperties(size="medium"),
         }
     return _lbp
+
 
 _lbp = {}
 
@@ -376,21 +407,22 @@ if __name__ == "__main__":
     class MyApp(wx.App):
         def OnInit(self):
             from numpy import arange, sin, pi, cos
-            'Create the main window and insert the custom frame'
-            x = arange(0.0,3.0,0.01)
-            s = sin(2*pi*x)
-            c = cos(2*pi*x)
-            t = sin(2*pi*x) + cos(2*pi*x)
+
+            "Create the main window and insert the custom frame"
+            x = arange(0.0, 3.0, 0.01)
+            s = sin(2 * pi * x)
+            c = cos(2 * pi * x)
+            t = sin(2 * pi * x) + cos(2 * pi * x)
             frame = ExtendedPlotFrame(None)
-            style = {'with':'lines', 'color':'blue','line':'solid','width':2}
-            style['legend'] = 'sin(x)'
-            frame.insertCurve(x,s, style)
-            style = {'with':'lines', 'color':'red','line':'solid','width':2}
-            style['legend'] = 'cos(x)'
-            frame.insertCurve(x,c, style)
-            style = {'with':'lines', 'color':'black','line':'solid','width':2}
-            #style['legend'] = 'sin(x)+cos(x)'
-            frame.insertCurve(x,t, style)
+            style = {"with": "lines", "color": "blue", "line": "solid", "width": 2}
+            style["legend"] = "sin(x)"
+            frame.insertCurve(x, s, style)
+            style = {"with": "lines", "color": "red", "line": "solid", "width": 2}
+            style["legend"] = "cos(x)"
+            frame.insertCurve(x, c, style)
+            style = {"with": "lines", "color": "black", "line": "solid", "width": 2}
+            # style['legend'] = 'sin(x)+cos(x)'
+            frame.insertCurve(x, t, style)
             frame.Show(True)
             return True
 
