6d47842327884a2764ee3ac06f5338ec79fa6a4a
Caden Myers <cjm2304@columbia.edu>
2024-07-12 11:07:01 -0400

    black edits 1 in doc/

--- src/diffpy/pdfgui/doc/manual/extractEquations.py
+++ src/diffpy/pdfgui/doc/manual/extractEquations.py
@@ -8,10 +8,10 @@ directory.
 # constants
 
 rc = {
-        'directory' : 'images',     # output directory
-        'resolution' : 72,          # equation images resolution
-        'eqns' : [],                # list of raw equation codes
-        'tmpdir' : None,            # temporary directory
+    "directory": "images",  # output directory
+    "resolution": 72,  # equation images resolution
+    "eqns": [],  # list of raw equation codes
+    "tmpdir": None,  # temporary directory
 }
 
 eqlatex = r"""
@@ -35,6 +35,7 @@ import sys
 import os
 import shutil
 
+
 def loadEquations():
     """Search for equation codes preceded by @EquationMark macro.
     Store equation codes in rc['eqns'].
@@ -48,7 +49,7 @@ def loadEquations():
     attex = False
     eqlines = []
     for line in lines:
-        bareline = line.strip().rstrip('{}')
+        bareline = line.strip().rstrip("{}")
         if bareline == eqmark:
             atmark = True
             continue
@@ -58,34 +59,36 @@ def loadEquations():
         elif attex and bareline == "@end tex":
             atmark = False
             attex = False
-            eq = ''.join(eqlines) + '\n'
-            rc['eqns'].append(eq)
+            eq = "".join(eqlines) + "\n"
+            rc["eqns"].append(eq)
             eqlines = []
         elif attex:
             eqlines.append(line)
     return
 
+
 def writePNGFiles():
     from tempfile import mkdtemp
-    rc['tmpdir'] = mkdtemp()
-    rc['directory'] = os.path.abspath(rc['directory'])
-    neqn = len(rc['eqns'])
+
+    rc["tmpdir"] = mkdtemp()
+    rc["directory"] = os.path.abspath(rc["directory"])
+    neqn = len(rc["eqns"])
     for i in range(neqn):
         fname = "eq-%02i.tex" % (i + 1)
-        fpath = os.path.join(rc['tmpdir'], fname)
-        fhandle = open(fpath, 'w')
-        s = eqlatex % rc['eqns'][i]
+        fpath = os.path.join(rc["tmpdir"], fname)
+        fhandle = open(fpath, "w")
+        s = eqlatex % rc["eqns"][i]
         fhandle.write(s)
         fhandle.close()
         convertToPNG(fpath)
         pngsrc = fpath[:-4] + ".png"
-        pngdst = os.path.join(rc['directory'], fname[:-4] + ".png")
+        pngdst = os.path.join(rc["directory"], fname[:-4] + ".png")
         shutil.copyfile(pngsrc, pngdst)
     return
 
+
 def convertToPNG(texfile):
-    """Compile texfile and convert it to PNG.
-    """
+    """Compile texfile and convert it to PNG."""
     os.chdir(os.path.dirname(texfile))
     texbasename = os.path.splitext(os.path.basename(texfile))[0]
     cmd = "latex --interaction nonstopmode %r" % texbasename
@@ -95,17 +98,22 @@ def convertToPNG(texfile):
     psfilename = texbasename + ".ps"
     bb = getBoundingBox(psfilename)
     pgbb = getPageBoundingBox(psfilename)
-    pt2px = rc['resolution'] / 72.0
+    pt2px = rc["resolution"] / 72.0
     xpx = pt2px * bb[0]
     ypx = pt2px * (pgbb[3] - bb[3])
     wpx = pt2px * (bb[2] - bb[0])
     hpx = pt2px * (bb[3] - bb[1])
     geometry = "%ix%i+%i+%i" % (wpx, hpx, xpx, ypx)
     pngfilename = texbasename + ".png"
-    cmd = "convert -strip -density %i %r -crop %s +repage %r" % \
-        (rc['resolution'], psfilename, geometry, pngfilename)
+    cmd = "convert -strip -density %i %r -crop %s +repage %r" % (
+        rc["resolution"],
+        psfilename,
+        geometry,
+        pngfilename,
+    )
     os.system(cmd) != 0 and sys.exit(1)
     return
+
 
 def getBoundingBox(psfilename):
     """Run ghostscript to obtain effective bounding box of psfilename.
@@ -119,24 +127,28 @@ def getBoundingBox(psfilename):
     o.close()
     out += e.read()
     e.close()
-    bb = [ int(w) for w in out.split()[1:5] ]
+    bb = [int(w) for w in out.split()[1:5]]
     return bb
+
 
 def getPageBoundingBox(psfilename):
     """Obtain bounding box value defined in psfilename.
     Return a list of bounding box coordinates.
     """
     import re
+
     with open(psfilename) as fp:
         s = fp.read()
-    bbline = re.search('^%%BoundingBox: *(.*)$', s, re.M)
-    pgbb = [ int(w) for w in bbline.group(1).split()[:4] ]
+    bbline = re.search("^%%BoundingBox: *(.*)$", s, re.M)
+    pgbb = [int(w) for w in bbline.group(1).split()[:4]]
     return pgbb
+
 
 def main():
     loadEquations()
     writePNGFiles()
-    shutil.rmtree(rc['tmpdir'])
+    shutil.rmtree(rc["tmpdir"])
+
 
 if __name__ == "__main__":
     main()
