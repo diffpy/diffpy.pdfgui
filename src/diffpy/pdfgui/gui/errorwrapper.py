9feb7b4eaf6e47d5c383b22abb8f22cf8d100373
Caden Myers <cjm2304@columbia.edu>
2024-07-25 12:33:27 -0600

    update copyright

--- src/diffpy/pdfgui/gui/errorwrapper.py
+++ src/diffpy/pdfgui/gui/errorwrapper.py
@@ -3,7 +3,7 @@
 #
 # PDFgui            by DANSE Diffraction group
 #                   Simon J. L. Billinge
-#                   (c) 2008 trustees of the Michigan State University.
+#                   (c) 2008-2024 trustees of the Michigan State University.
 #                   All rights reserved.
 #
 # File coded by:    Chris Farrow
