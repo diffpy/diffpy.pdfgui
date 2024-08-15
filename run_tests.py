7bf06c4a00a7461f72d9cc3f52e05496cd732175
Tieqiong Zhang <tz2600@columbia.edu>
2024-08-15 19:42:32 -0400

    try to remove no test ran line

--- run_tests.py
+++ run_tests.py
@@ -2,5 +2,9 @@
 
 import diffpy.pdfgui.tests
 
-assert diffpy.pdfgui.tests.test().wasSuccessful()
-assert diffpy.pdfgui.tests.testdeps().wasSuccessful()
+def run_tests():
+    assert diffpy.pdfgui.tests.test().wasSuccessful()
+    assert diffpy.pdfgui.tests.testdeps().wasSuccessful()
+
+if __name__ == '__main__':
+    run_tests()
